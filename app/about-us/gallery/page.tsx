1dccc2897cc2ad2d678abb2239f2b956c6a2a110
NjorogeLab <kenable72@gmail.com>
2024-02-16 00:21:03 +1100

    fixed commented gallery

--- app/about-us/gallery/page.tsx
+++ app/about-us/gallery/page.tsx
@@ -100,3 +100,15 @@
 // };
 
 // export default Home;
+
+import {FC} from 'react'
+
+interface pageProps{
+
+}
+
+const page: FC<pageProps> = ({}) => {
+   return <div>page</div>
+}
+
+export default page
